658f19c34593d2501a4541114d332a437adf7099
stuewe.matthew@gmail.com <stuewe.matthew@gmail.com>
2019-03-14 10:12:40 +0000

    Users added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+   before_action :set_article, only: [:edit, :update, :show, :destroy]
+   
     
   def new
      @article=Article.new
@@ -8,25 +10,24 @@ class ArticlesController < ApplicationController
       @article=Article.new(article_params)
       if @article.save
         redirect_to article_path(@article)
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
       else
          render :new
       end
   end
   
   def edit
-    @article=Article.find(params[:id])
+    
   end
   
   def show
-    @article=Article.find(params[:id])
+    
   end
   
   def update
-    @article= Article.find(params[:id])
     if @article.update(article_params)
-      flash[:notice]="Article updated!"
       redirect_to article_path(@article)
+      flash[:success]="Article updated!"
     else
       render :edit
     end
@@ -36,10 +37,10 @@ class ArticlesController < ApplicationController
     @articles=Article.all
   end
   
-  def destroy 
-    @article = Article.find@params[:id]
+  def destroy
+    
     @article.destroy
-    flash[:notice]="Article deleted"
+    flash[:success]="Article deleted"
     redirect_to articles_path
   end
   
@@ -47,5 +48,9 @@ class ArticlesController < ApplicationController
     def article_params
         params.require(:article).permit(:title, :description)
     end
- 
+  
+    def set_article
+      @article = Article.find(params[:id])
+    end
+   
 end
