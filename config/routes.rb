36b444dbdc18098676e48cceead4cc7e08d9fb90
stuewe.matthew@gmail.com <stuewe.matthew@gmail.com>
2019-03-18 04:05:37 +0000

    Added edit user functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
-  
-  root 'pages#home'
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
+   root 'pages#home'
   get 'about', to: 'pages#about'
 end
